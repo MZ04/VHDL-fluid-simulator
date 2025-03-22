463c4025f30d094a9962aa0f50957c3acf5943ce
Marco <marco.zanatta@studenti.unitn.it>
2025-03-22 15:45:57 +0100

    added matrix driver's main feature

--- Fluid_dynamic.srcs/sources_1/new/display_driver.vhd
+++ Fluid_dynamic.srcs/sources_1/new/display_driver.vhd
@@ -33,19 +33,50 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity display_driver is
   Port (clk : in std_logic;
-        frame : in std_logic_vector (511 downto 0);
+        reset_in : in std_logic;
+        processed_frame : in std_logic_vector (511 downto 0);
         frame_ready : in std_logic;
-        red_data : out std_logic;
-        green_data : out std_logic;
-        select_out : out std_logic;
-        bright : out std_logic;
-        reset_display : out std_logic
+        copy_done : out std_logic;
+        red_data, green_data, select_out, reset_display, bright : out std_logic
         );
 end display_driver;
 
 architecture Behavioral of display_driver is
-    signal swapped : std_logic_vector (511 downto 0);
-    signal display_ready : std_logic;
+    signal frame_buffer: std_logic_vector (511 downto 0);
+    signal buffer_available : std_logic;
 begin
-                     
+    -- The code is designed to ignore the select signal
+    select_out <= '0';
+    
+    -- Reset is handled by the external system 
+    reset_display <= reset_in;
+    
+    -- New frame is loaded in the buffer
+    process (frame_ready, buffer_available)
+    begin
+        if (frame_ready = '1' and buffer_available = '1') then 
+            frame_buffer <= processed_frame;
+            buffer_available <= '0';
+            copy_done <= '1'; -- When the processed frame is copied in the buffer the system that calculates the following frame is notified
+        end if;
+    end process;
+    
+    --New frame is displayed in the matrix
+    process (buffer_available, clk)
+        variable count : integer := 0;
+    begin
+        if (buffer_available = '0') then 
+            if (rising_edge(clk)) then 
+                if (count < 512) then 
+                    red_data <= frame_buffer (count);
+                    green_data <= frame_buffer (count + 1);
+                    count := count + 2;
+                else 
+                    count := 0;
+                    buffer_available <= '1';
+                end if;
+            end if;
+        end if;
+    end process;
+    
 end Behavioral;
