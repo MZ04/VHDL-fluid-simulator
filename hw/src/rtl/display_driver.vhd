bcdf3e22a824a2c67f36f2f96a23f3217aae9c5a
Marco <marco.zanatta@studenti.unitn.it>
2025-03-29 17:40:24 +0100

    nearly completed display driver (no testbench)

--- hw/src/rtl/display_driver.vhd
+++ hw/src/rtl/display_driver.vhd
@@ -32,18 +32,28 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity display_driver is
-  Port (clk : in std_logic;
+  Port (clk_in : in std_logic;
         reset_in : in std_logic;
         processed_frame : in std_logic_vector (511 downto 0);
         frame_ready : in std_logic;
         copy_done : out std_logic;
-        red_data, green_data, select_out, reset_display, bright : out std_logic
+        red_data, green_data, select_out, reset_display, bright, clk_out : out std_logic
         );
 end display_driver;
 
 architecture Behavioral of display_driver is
     signal frame_buffer: std_logic_vector (511 downto 0);
     signal buffer_available : std_logic;
+    type state is (
+        idle,
+        tbrf_tsesu,
+        first_led,
+        fill_row,
+        add_clock,
+        tbrb,
+        tcke,
+        add_clock_2
+    );
 begin
     -- The code is designed to ignore the select signal
     select_out <= '0';
@@ -62,20 +72,73 @@ begin
     end process;
     
     --New frame is displayed in the matrix
-    process (buffer_available, clk)
-        variable count : integer := 0;
+    process (buffer_available, clk_in)
+        variable debug_count : integer := 0;
+        variable pixel_count : integer := 0;
+        variable row_count : integer := 0; 
+        variable current_state : state := idle;
     begin
-        if (buffer_available = '0') then 
-            if (rising_edge(clk)) then 
-                if (count < 512) then 
-                    red_data <= frame_buffer (count);
-                    green_data <= frame_buffer (count + 1);
-                    count := count + 2;
-                else 
-                    count := 0;
-                    buffer_available <= '1';
+        if (buffer_available = '0') then
+            clk_out <= '0'; 
+            bright <= '0';
+            if (rising_edge(clk_in)) then
+                case current_state is 
+                when idle => 
+                    debug_count := 0;
+                    reset_display <= '1';
+                    select_out <= '0';
+                    if (reset_in = '0') then
+                            current_state := tbrf_tsesu;
+                    end if; 
+                when tbrf_tsesu =>  
+                    debug_count := debug_count + 1;
+                    if (debug_count = 140) then
+                        current_state := first_led;
+                        debug_count := 0;
+                    end if;
+                when first_led =>
+                    red_data <= frame_buffer((row_count * 16) + pixel_count);
+                    green_data <= frame_buffer((row_count * 16) + pixel_count + 1);
+                    pixel_count := pixel_count + 2;
+                    current_state := fill_row;
+                when fill_row => 
+                    clk_out <= '1';
+                    if (pixel_count > 16) then 
+                        current_state := add_clock;
+                        pixel_count := 0;
+                    end if; 
+                when add_clock =>
+                    clk_out <= '1';
+                    row_count := row_count + 1;
+                    if (row_count = 16) then 
+                        current_state := add_clock_2;
+                    else 
+                        current_state := tbrb;
+                    end if; 
+                when tbrb => 
+                    debug_count := debug_count + 1;
+                    if (debug_count = 88) then 
+                        debug_count := 0;
+                        current_state := tcke;
+                    end if;
+                when tcke =>
+                    bright <= '1';
+                    debug_count := debug_count + 1;
+                    if (debug_count = 99) then -- TODO: Insert value based on an estimation
+                        debug_count := 0;
+                        current_state := idle;
+                    end if;
+                when add_clock_2 =>
+                    clk_out <= '1';
+                    current_state := idle;
+                end case;
+            else 
+                if (current_state = fill_row) then 
+                    red_data <= frame_buffer((row_count * 16) + pixel_count);
+                    green_data <= frame_buffer((row_count * 16) + pixel_count + 1);
                 end if;
             end if;
+        
         end if;
     end process;
     
