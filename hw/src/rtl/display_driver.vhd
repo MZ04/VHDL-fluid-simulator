9207c2b60202d3b38eda39da74ca55f2e0425d5f
Marco <marco.zanatta@studenti.unitn.it>
2025-04-01 17:22:47 +0200

    display driver + testbench

--- hw/src/rtl/display_driver.vhd
+++ hw/src/rtl/display_driver.vhd
@@ -43,7 +43,7 @@ end display_driver;
 
 architecture Behavioral of display_driver is
     signal frame_buffer: std_logic_vector (511 downto 0);
-    signal buffer_available : std_logic;
+    signal buffer_drawn : std_logic;
     type state is (
         idle,
         tbrf_tsesu,
@@ -54,92 +54,104 @@ architecture Behavioral of display_driver is
         tcke,
         add_clock_2
     );
-begin
-    -- The code is designed to ignore the select signal
-    select_out <= '0';
-    
-    -- Reset is handled by the external system 
-    reset_display <= reset_in;
-    
-    -- New frame is loaded in the buffer
-    process (frame_ready, buffer_available)
     begin
-        if (frame_ready = '1' and buffer_available = '1') then 
+        
+    -- Select is set as high since we are feeding the display with data    
+    select_out <= '1';
+
+    -- New frame is loaded in the buffer
+    process (frame_ready, buffer_drawn)
+    begin
+        if (frame_ready = '1' and buffer_drawn = '0') then 
             frame_buffer <= processed_frame;
-            buffer_available <= '0';
+            buffer_drawn <= '1';
             copy_done <= '1'; -- When the processed frame is copied in the buffer the system that calculates the following frame is notified
         end if;
     end process;
     
     --New frame is displayed in the matrix
-    process (buffer_available, clk_in)
+    process (clk_in, reset_in)
         variable debug_count : integer := 0;
         variable pixel_count : integer := 0;
         variable row_count : integer := 0; 
         variable current_state : state := idle;
     begin
-        if (buffer_available = '0') then
-            clk_out <= '0'; 
-            bright <= '0';
-            if (rising_edge(clk_in)) then
-                case current_state is 
-                when idle => 
-                    debug_count := 0;
-                    reset_display <= '1';
-                    select_out <= '0';
-                    if (reset_in = '0') then
-                            current_state := tbrf_tsesu;
-                    end if; 
-                when tbrf_tsesu =>  
-                    debug_count := debug_count + 1;
-                    if (debug_count = 140) then
-                        current_state := first_led;
-                        debug_count := 0;
-                    end if;
-                when first_led =>
-                    red_data <= frame_buffer((row_count * 16) + pixel_count);
-                    green_data <= frame_buffer((row_count * 16) + pixel_count + 1);
-                    pixel_count := pixel_count + 2;
-                    current_state := fill_row;
-                when fill_row => 
-                    clk_out <= '1';
-                    if (pixel_count > 16) then 
-                        current_state := add_clock;
-                        pixel_count := 0;
-                    end if; 
-                when add_clock =>
-                    clk_out <= '1';
-                    row_count := row_count + 1;
-                    if (row_count = 16) then 
-                        current_state := add_clock_2;
-                    else 
-                        current_state := tbrb;
-                    end if; 
-                when tbrb => 
-                    debug_count := debug_count + 1;
-                    if (debug_count = 88) then 
-                        debug_count := 0;
-                        current_state := tcke;
-                    end if;
-                when tcke =>
-                    bright <= '1';
-                    debug_count := debug_count + 1;
-                    if (debug_count = 99) then -- TODO: Insert value based on an estimation
-                        debug_count := 0;
-                        current_state := idle;
-                    end if;
-                when add_clock_2 =>
-                    clk_out <= '1';
-                    current_state := idle;
-                end case;
-            else 
-                if (current_state = fill_row) then 
-                    red_data <= frame_buffer((row_count * 16) + pixel_count);
-                    green_data <= frame_buffer((row_count * 16) + pixel_count + 1);
-                end if;
-            end if;
-        
+        --Signals which are set by default
+        clk_out <= '0'; 
+        bright <= '0';
+        reset_display <= '0';
+
+        --reset_in needs to be asynchronous
+        if (reset_in = '1') then
+            current_state := idle;
+            buffer_drawn <= '0';
         end if;
+
+
+        if (rising_edge(clk_in)) then
+            case current_state is 
+            when idle => --Initial state
+                debug_count := 0;
+                reset_display <= '1';
+                if (buffer_drawn = '1') then
+                    current_state := tbrf_tsesu;
+                end if; 
+
+            when tbrf_tsesu => --Defined for   
+                debug_count := debug_count + 1;
+                if (debug_count = 140) then
+                    current_state := first_led;
+                    debug_count := 0;
+                end if;
+
+            when first_led =>
+                red_data <= frame_buffer((row_count * 32) + pixel_count);
+                green_data <= frame_buffer((row_count * 32) + pixel_count + 1);
+                pixel_count := pixel_count + 2;
+                current_state := fill_row;
+
+            when fill_row => 
+                clk_out <= '1';
+                if (pixel_count > 28) then 
+                    current_state := add_clock;
+                    pixel_count := 0;
+                end if; 
+
+            when add_clock =>
+                clk_out <= '1';
+                row_count := row_count + 1;
+                if (row_count = 16) then 
+                    current_state := add_clock_2;
+                else 
+                    current_state := tbrb;
+                end if; 
+
+            when tbrb => 
+                debug_count := debug_count + 1;
+                if (debug_count = 88) then 
+                    debug_count := 0;
+                    current_state := tcke;
+                end if;
+
+            when tcke =>
+                bright <= '1';
+                debug_count := debug_count + 1;
+                if (debug_count = 99) then -- TODO: Insert value based on an estimation
+                    debug_count := 0;
+                    current_state := idle;
+                end if;
+
+            when add_clock_2 =>
+                clk_out <= '1';
+                current_state := idle;
+            end case;
+        else 
+            if (current_state = fill_row) then 
+                red_data <= frame_buffer((row_count * 16) + pixel_count);
+                green_data <= frame_buffer((row_count * 16) + pixel_count + 1);
+                pixel_count := pixel_count + 2;
+            end if;
+        end if;    
     end process;
     
 end Behavioral;
---
+++ hw/src/tb/display_driver_testbench.vhd
@@ -0,0 +1,93 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/13/2025 05:41:43 PM
+-- Design Name: 
+-- Module Name: clock_div_testbench - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+library xil_defaultlib;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity display_driver_testbench is   
+--  Port ( );
+end display_driver_testbench;
+
+architecture Structural of display_driver_testbench is
+    signal clk_in, reset_in, frame_ready, copy_done: std_logic;
+    signal red_data, green_data, select_out, reset_display, bright, clk_out : std_logic;
+    signal processed_frame : std_logic_vector (511 downto 0);
+begin
+    dd: entity xil_defaultlib.display_driver port map (
+        clk_in => clk_in,
+        reset_in => reset_in,
+        processed_frame => processed_frame,
+        frame_ready => frame_ready,
+        copy_done => copy_done,
+        red_data => red_data,
+        green_data => green_data,
+        select_out => select_out,
+        reset_display => reset_display,
+        bright => bright,
+        clk_out => clk_out
+    );
+
+    clock_process : process 
+    begin 
+        clk_in <= '1';
+        wait for 25 ns;
+        clk_in <= '0';
+        wait for 25 ns;
+    end process;
+
+    reset_process : process
+    begin 
+        reset_in <= '1';
+        wait for 100 ns;
+        reset_in <= '0';
+        wait;
+    end process;
+
+    set_processed_frame : process
+        variable temp : std_logic_vector (511 downto 0);
+    begin 
+        for i in 0 to 255 loop
+            temp(2*i) := '0';
+            temp(2*i+1) := '1';
+        end loop;
+        processed_frame <= temp;
+        wait;
+    end process;
+
+    set_frame_ready : process
+    begin 
+        frame_ready <= '0';
+        wait for 50 ns;
+        frame_ready <= '1';
+        wait;
+    end process;
+
+end Structural;
